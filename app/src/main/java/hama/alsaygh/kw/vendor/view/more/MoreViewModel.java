a7ba3d3f3bf0ec3bb7e0e08e1ebebb44cb965496
walaaabushaban <walaa@brisk-drive.com>
2022-06-28 08:41:01 +0400

    -

--- app/src/main/java/hama/alsaygh/kw/vendor/view/more/MoreViewModel.java
+++ app/src/main/java/hama/alsaygh/kw/vendor/view/more/MoreViewModel.java
@@ -2,6 +2,7 @@ package hama.alsaygh.kw.vendor.view.more;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.view.View;
 
 import androidx.lifecycle.ViewModel;
@@ -14,7 +15,6 @@ import hama.alsaygh.kw.vendor.view.contactUs.ContactUsActivity;
 import hama.alsaygh.kw.vendor.view.generalSettings.GeneralSettingsActivity;
 import hama.alsaygh.kw.vendor.view.mySales.MySalesActivity;
 import hama.alsaygh.kw.vendor.view.profile.ProfileActivity;
-import hama.alsaygh.kw.vendor.view.rateHama.RateHamaActivity;
 import hama.alsaygh.kw.vendor.view.recommendation.RecommendationActivity;
 import hama.alsaygh.kw.vendor.view.storeRate.StoreRateActivity;
 import hama.alsaygh.kw.vendor.view.terms.TermsActivity;
@@ -33,11 +33,11 @@ public class MoreViewModel extends ViewModel {
     }
 
     public String getEmail() {
-        return  SharedPreferenceConstant.getSharedPreferenceUser(context).getEmail();
+        return SharedPreferenceConstant.getSharedPreferenceUser(context).getEmail();
     }
 
     public String getLogo() {
-        return  SharedPreferenceConstant.getSharedPreferenceUser(context).getLogo();
+        return SharedPreferenceConstant.getSharedPreferenceUser(context).getLogo();
     }
 
     public void onMySalesClick(View view) {
@@ -53,6 +53,7 @@ public class MoreViewModel extends ViewModel {
         Intent intent = new Intent(view.getContext(), StoreRateActivity.class);
         view.getContext().startActivity(intent);
     }
+
     public void onGeneralSettingsClick(View view) {
 
         Intent intent = new Intent(view.getContext(), GeneralSettingsActivity.class);
@@ -60,31 +61,43 @@ public class MoreViewModel extends ViewModel {
     }
 
     public void onHamaRateClick(View view) {
-        Intent intent = new Intent(view.getContext(), RateHamaActivity.class);
-        view.getContext().startActivity(intent);
+//        Intent intent = new Intent(view.getContext(), RateHamaActivity.class);
+//        view.getContext().startActivity(intent);
+
+        final String appPackageName = view.getContext().getPackageName(); // getPackageName() from Context or Activity object
+        try {
+            view.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+        } catch (android.content.ActivityNotFoundException anfe) {
+            view.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+        }
     }
 
     public void onTermsClick(View view) {
         Intent intent = new Intent(view.getContext(), TermsActivity.class);
         view.getContext().startActivity(intent);
     }
-     public void onContactHamaClick(View view) {
-         Intent intent = new Intent(view.getContext(), ContactUsActivity.class);
-         view.getContext().startActivity(intent);
+
+    public void onContactHamaClick(View view) {
+        Intent intent = new Intent(view.getContext(), ContactUsActivity.class);
+        view.getContext().startActivity(intent);
     }
-     public void onRecommendationHamaClick(View view) {
-         Intent intent = new Intent(view.getContext(), RecommendationActivity.class);
-         view.getContext().startActivity(intent);
+
+    public void onRecommendationHamaClick(View view) {
+        Intent intent = new Intent(view.getContext(), RecommendationActivity.class);
+        view.getContext().startActivity(intent);
 
     }
+
     public void onAppointmentHamaClick(View view) {
         Intent intent = new Intent(view.getContext(), AppointmentBookingActivity.class);
         view.getContext().startActivity(intent);
     }
+
     public void onLogoutClick(View view) {
 
         Utils.getInstance().logOut(view.getContext());
     }
+
     public void onEditProfileClick(View view) {
         Intent intent = new Intent(view.getContext(), ProfileActivity.class);
         view.getContext().startActivity(intent);
